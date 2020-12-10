931f9615c9b0042db2f84ee1316e0fd2434ccb4d
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-10 13:07:56 +0200

    more usage of Last

--- Program.cs
+++ Program.cs
@@ -9,10 +9,12 @@ namespace _16.LinqLast
         static void Main(string[] args)
         {
             int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
-            Console.WriteLine(numbers.Last());  //give us last element of the collection
-            Console.WriteLine(numbers.Last(x => x % 2 == 0)); // give us the last elemnt in the array that divided by 2 give us reminder =0
+           // Console.WriteLine(numbers.Last());  //give us last element of the collection
+           // Console.WriteLine(numbers.Last(x => x % 2 == 0)); // give us the last elemnt in the array that divided by 2 give us reminder =0
             // ie give us the last even number of the array numbers
-            Console.WriteLine(numbers.Last(x => x % 2 != 0)); //give me the last number of the array that is not even, that is odd !
+           // Console.WriteLine(numbers.Last(x => x % 2 != 0)); //give me the last number of the array that is not even, that is odd !
+            Console.WriteLine(numbers.Last(x => x < 4 )); //give me the last element that is < 4 ;
+        
         }
     }
 }
