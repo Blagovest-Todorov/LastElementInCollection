57b4aae95fa55103fd7a75b376500ebd98235e9b
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-10 12:55:16 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _16.LinqLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] {1, 2 , 3 , 4 , 5 , 6  };
+            Console.WriteLine(numbers.Last());  //give us last element of the collection
+            Console.WriteLine(numbers.Last(x => x % 2 == 0)); // give us the last elemnt in the array that divided by 2 give us reminder =0
+            // ie give us the last even number of the array numbers
+        }
+    }
+}
