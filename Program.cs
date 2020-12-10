1bb3cdc5e4d2c53fef0afdcc59c9e903743e989a
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-10 12:58:23 +0200

    more examples

--- Program.cs
+++ Program.cs
@@ -8,10 +8,11 @@ namespace _16.LinqLast
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[] {1, 2 , 3 , 4 , 5 , 6  };
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
             Console.WriteLine(numbers.Last());  //give us last element of the collection
             Console.WriteLine(numbers.Last(x => x % 2 == 0)); // give us the last elemnt in the array that divided by 2 give us reminder =0
             // ie give us the last even number of the array numbers
+            Console.WriteLine(numbers.Last(x => x % 2 != 0)); //give me the last number of the array that is not even, that is odd !
         }
     }
 }
